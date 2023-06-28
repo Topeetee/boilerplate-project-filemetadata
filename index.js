689d6eea9d32b85c172295c344233d7fc519dd38
Topeetee <temitopeakinwekomi28@gmail.com>
2023-06-28 06:30:54 -0700

    commit made

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const multer = require('multer');
 var cors = require('cors');
 require('dotenv').config()
 
@@ -9,6 +10,35 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+
+// const storage = multer.diskStorage({
+//   destination: 'uploads/',
+//   filename: function (req, file, cb) {
+//     // Generate a unique file name
+//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+//     const fileName = file.originalname + '-' + uniqueSuffix;
+//     cb(null, fileName);
+//   },
+// });
+
+// Configure Multer with the storage option
+// const upload = multer({ storage });
+
+
+var upload = multer({ dest: 'uploads/' });
+
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  // File has been uploaded successfully
+  const { originalname, mimetype, size } = req.file;
+  try{
+    res.json({ filename: originalname, type: mimetype, size });
+  }catch(err){
+    res.json(err)
+  }
+  
 });
 
 
